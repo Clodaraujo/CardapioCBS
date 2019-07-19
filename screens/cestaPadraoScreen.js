4977d2b98ff11f7c47257787972ecb9fdee756c0
Pedro <pedromiotti7@gmail.com>
2019-07-19 17:32:44 -0300

    19/07

--- screens/cestaPadraoScreen.js
+++ screens/cestaPadraoScreen.js
@@ -1,13 +1,24 @@
  //cestaPadrao Screen 
  import React, { Component } from 'react';
- import{ StyleSheet, View, Text, Image, ScrollView } from 'react-native';
+ import{ StyleSheet, View, Text, Image, ScrollView, Dimensions } from 'react-native';
  
  import CestasCardsScreen from '../screens/cestasCardsScreen'
 
+ const { height } = Dimensions.get('window');
  
  const URL_CESTAS = 'http://192.168.0.105:3000/cestas'
  
  export default class cestaPadrao extends Component{
+  // Enabling ScrollView to the end of the page
+  state = {
+    screenHeight: height,
+  };
+
+  onContentSizeChange = (contentWidth, contentHeight) => {
+    this.setState({ screenHeight: contentHeight });
+  };
+
+  // Inserting the JSON file into the mock server
      constructor(props) {
          super(props);
      
@@ -29,19 +40,23 @@
        
  
      render(){
-      //  console.log(this.state.cestas);
+
+      const scrollEnabled = this.state.screenHeight > height;
+      
          return(
           <View style = { style.container }>
-
-              {/* Logo + Retangulo -------
+            
+            {/* Logo + Retangulo ------- */}
               <View style = {style.header}>
                   <View style = {style.logocontainer}>
                       <Image style = {{width: 210, height: 210}} source = {require('../assets/CBSLogo.png')} resizeMode = 'contain'/>
                   </View>
-              </View> */}
-              <ScrollView horizontal={true} style={{flex: 1}}>
+              </View>
+
+              <ScrollView style={{flex: 1}} onContentSizeChange ={this.onContentSizeChange} scrollEnabled={scrollEnabled}>
                 <CestasCardsScreen todasCestas={this.state.cestas}/>
-              </ScrollView>
+            </ScrollView>
+            
           </View>
            
          )
@@ -56,27 +71,24 @@
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        
-        
-        // alignItems: 'center',
-        // justifyContent: 'space-between',
-        // backgroundColor:'#fff',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor:'#fff',
         
       },
     
-    // header:{
-    //     height: 1,
-    //     backgroundColor: '#EBF5DF',
-    //     width: 1280,
+    header:{
+        height: 1,
+        backgroundColor: '#EBF5DF',
+        width: 1280,
+        
   
-    // },
-    // logocontainer: {
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     backgroundColor: '#EBF5DF',
-    // },
-    // list:{
-    //   flex: 1
-    // },
+    },
+    logocontainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#EBF5DF',
+    },
+    
   
   });
--- screens/cestasCardsScreen.js
+++ screens/cestasCardsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import{ Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text, Icon, Card, CardItem, Footer } from 'native-base';
 
 const listaCestas = (props) => {
@@ -8,21 +8,29 @@ const listaCestas = (props) => {
       if(todasCestas) {
         return todasCestas.map((item) => {
           return (
-            <ListItem key={item.id}>
-              <Card style={{alignItems: 'center',width: 250 ,flex: 0}}>
+            <ListItem key={item.id} style={{padding: 25}} >
+
+              <Card style={{alignItems: 'center',width: 320, height: 400 ,flex: 0, borderRadius: 35, backgroundColor:'#F1F7F2'}}>
+
                 <CardItem cardBody>                
-                  
-                    <Thumbnail square source={{ uri: `${item.imagem}`}} style={{ height: 200, width: 100 , flex: 1}}/>
-                  
+                  <Thumbnail square source={{ uri: `${item.imagem}`}} style={{ height: 200, width: 200 ,backgroundColor:'#F1F7F2', marginTop:0, marginBottom: -25}}/>
+                </CardItem>
+
+                  <CardItem style={{backgroundColor:'#F1F7F2'}}>
+                    <Body style={{alignItems: 'center'}}>
+                      <Text style={{fontSize: 30, color: '#006E90', fontFamily: 'sans-serif-light', paddingBottom: 12}}>{`${item.nome_cesta}`}</Text>
+                      <Text note style={{fontSize: 25, color: '#006E90' , fontFamily: 'sans-serif-light',  paddingBottom: 10}}>{`${item.preco}`}</Text>
+                    </Body> 
+                  </CardItem >
+                      
+                  <CardItem style={{backgroundColor:'#F1F7F2'}}>
+                      <TouchableOpacity style={{width: 214}}>
+                          <Text style={{paddingTop:7,textAlign: 'center',fontSize: 23,color:"#fff", fontFamily: 'sans-serif-light', backgroundColor:'#006E90', borderColor: '#006E90', borderWidth: 2, borderRadius: 15, height: 49}}>Vizualizar </Text>
+                      </TouchableOpacity>
                   </CardItem>
 
-                  <CardItem >
-                    <Body>
-                      <Text>{`${item.nome_cesta}`}</Text>
-                      <Text note>{`${item.preco}`}</Text>
-                    </Body> 
-                  </CardItem>
                 </Card>
+
             </ListItem>
           )
         })
@@ -30,9 +38,8 @@ const listaCestas = (props) => {
   }
   return(
       <Container>
-        <Header transparent />
         <Content padder>
-          <List horizontal={true} style={{flex: 1}}>  
+          <List style={{ flex: 1,flexDirection: 'row', flexWrap: 'wrap'}}>  
               {lista(props)}
           </List>
         </Content>
